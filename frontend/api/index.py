9e5b1f3e2c4cee03da87ca04e48d13150e670bcc
Talish-creator <jsttalish@gmail.com>
2026-06-17 00:33:06 +0530

    xchanges

--- frontend/api/index.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Add the backend directory to sys.path so we can import 'server'
-backend_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'backend'))
-if backend_path not in sys.path:
-    sys.path.insert(0, backend_path)
-
-from server import app
