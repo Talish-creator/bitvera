939a1de4e4cd3005fd933a6a812814d335d1ec15
Talish-creator <jsttalish@gmail.com>
2026-06-17 01:25:42 +0530

    Fix partner logos and flatten translation JSONs to fix Arabic missing translations

--- frontend/src/mock/partners.js
+++ frontend/src/mock/partners.js
@@ -1,10 +1,10 @@
 export const partnersData = [
   { name: "ZATCA", logo: "https://flagcdn.com/w320/sa.png" },
-  { name: "AWS", logo: "https://upload.wikimedia.org/wikipedia/commons/9/93/Amazon_Web_Services_Logo.svg" },
-  { name: "Microsoft", logo: "https://upload.wikimedia.org/wikipedia/commons/9/96/Microsoft_logo_%282012%29.svg" },
-  { name: "Google Cloud", logo: "https://upload.wikimedia.org/wikipedia/commons/5/51/Google_Cloud_logo.svg" },
-  { name: "SAP", logo: "https://upload.wikimedia.org/wikipedia/commons/5/59/SAP_2011_logo.svg" },
-  { name: "Oracle", logo: "https://upload.wikimedia.org/wikipedia/commons/5/50/Oracle_logo.svg" },
-  { name: "IBM", logo: "https://upload.wikimedia.org/wikipedia/commons/5/51/IBM_logo.svg" },
-  { name: "Salesforce", logo: "https://upload.wikimedia.org/wikipedia/commons/f/f9/Salesforce.com_logo.svg" }
+  { name: "AWS", logo: "https://cdn.simpleicons.org/amazonaws/232F3E" },
+  { name: "Microsoft", logo: "https://cdn.simpleicons.org/microsoft/00A4EF" },
+  { name: "Google Cloud", logo: "https://cdn.simpleicons.org/googlecloud/4285F4" },
+  { name: "SAP", logo: "https://cdn.simpleicons.org/sap/008FD3" },
+  { name: "Oracle", logo: "https://cdn.simpleicons.org/oracle/F80000" },
+  { name: "IBM", logo: "https://cdn.simpleicons.org/ibm/0530AD" },
+  { name: "Salesforce", logo: "https://cdn.simpleicons.org/salesforce/00A1E0" }
 ];
