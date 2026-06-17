cc217b2c7c196ac0460b93e172b3692c9ec12104
Talish-creator <jsttalish@gmail.com>
2026-06-17 16:50:25 +0530

    logo

--- frontend/src/mock/partners.js
+++ frontend/src/mock/partners.js
@@ -1,10 +1,10 @@
 export const partnersData = [
-  { name: "ZATCA", logo: "https://flagcdn.com/w320/sa.png" },
-  { name: "AWS", logo: "https://cdn.simpleicons.org/amazonaws/232F3E" },
-  { name: "Microsoft", logo: "https://cdn.simpleicons.org/microsoft/00A4EF" },
-  { name: "Google Cloud", logo: "https://cdn.simpleicons.org/googlecloud/4285F4" },
-  { name: "SAP", logo: "https://cdn.simpleicons.org/sap/008FD3" },
-  { name: "Oracle", logo: "https://cdn.simpleicons.org/oracle/F80000" },
-  { name: "IBM", logo: "https://cdn.simpleicons.org/ibm/0530AD" },
-  { name: "Salesforce", logo: "https://cdn.simpleicons.org/salesforce/00A1E0" }
+  { name: "ZATCA", logo: "/logos/sa.png" },
+  { name: "AWS", logo: "/logos/aws.svg" },
+  { name: "Microsoft", logo: "/logos/microsoft.svg" },
+  { name: "Google Cloud", logo: "/logos/googlecloud.svg" },
+  { name: "SAP", logo: "/logos/sap.svg" },
+  { name: "Oracle", logo: "/logos/oracle.svg" },
+  { name: "IBM", logo: "/logos/ibm.svg" },
+  { name: "Salesforce", logo: "/logos/salesforce.svg" }
 ];
